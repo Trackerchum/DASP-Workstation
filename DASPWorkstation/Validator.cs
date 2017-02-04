34d0d6ccb4b9d77f2f4eeb96f8545b4568db1f20
James Morrison <James Morrison>
2017-02-04 21:56:01 +0000

    little bits

--- DASPWorkstation/Validator.cs
+++ DASPWorkstation/Validator.cs
@@ -8,6 +8,24 @@ namespace DASPWorkstation
 {
     class Validator
     {
+        public enum ValidatorStatusCode
+        {
+            OK,
+            
+            AMPLITUDE_NOT_NUMERIC,
+            AMPLITUDE_NOT_WITHIN_PARAMETERS,
+            AMPLITUDE_IS_NULL,
 
+            FREQUENCY_NOT_NUMERIC,
+            FREQUENCY_NOT_WITHIN_PARAMETERS,
+            FREQUENCY_IS_NULL,
+
+            PHASE_NOT_NUMERIC,
+            PHASE_NOT_WITHIN_PARAMETERS,
+            PHASE_IS_NULL,
+            
+            NO_SINE_SELECTED_TO_EDIT,
+            NO_SIGNAL_TO_PLOT
+        }
     }
 }
