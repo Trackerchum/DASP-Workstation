1d1a8bd351b55d123e6bbd096259f0563687cf05
James Morrison <James Morrison>
2017-02-15 21:27:40 +0000

    bit reversal algorithm programmed and tested

---
+++ DASPWorkstation.Test/FFT_RadixTwoSpec.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DASPWorkstation.Test
+{
+    [TestClass]
+    public class FFT_RadixTwoSpec
+    {
+        private FFT_RadixTwo fft = new FFT_RadixTwo();
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesTwo()
+        {
+            int N = 2;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesFour()
+        {
+            int N = 4;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesEight()
+        {
+            int N = 8;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesSixteen()
+        {
+            int N = 16;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesThirtyTwo()
+        {
+            int N = 32;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesSixtyFour()
+        {
+            int N = 64;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesOneTwoEight()
+        {
+            int N = 128;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesTwoFiveSix()
+        {
+            int N = 256;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesFiveOneTwo()
+        {
+            int N = 512;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesTenTwentyFour()
+        {
+            int N = 1024;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesTwentyFortyEight()
+        {
+            int N = 2048;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesFortyNinetySix()
+        {
+            int N = 4096;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesEightyOneNinetyTwo()
+        {
+            int N = 8192;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesSixteenXXX()
+        {
+            int N = 16384;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectNumberOfValuesEightyThirtyTwoXXX()
+        {
+            int N = 32768;
+            var actual = fft.BitReversal(N);
+
+            Assert.AreEqual(N, actual.Count);
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectValuesEight()
+        {
+            int N = 8;
+            var actual = fft.BitReversal(N);
+            var expected = new List<int>(new int[] { 0, 4, 2, 6, 1, 5, 3, 7 });
+
+            for (int n = 0; n < 8; n++)
+            {
+                Assert.AreEqual(expected[n], actual[n]);
+            }
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectValuesSixteen()
+        {
+            int N = 16;
+            var actual = fft.BitReversal(N);
+            var expected = new List<int>(new int[] { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 });
+
+            for (int n = 0; n < 16; n++)
+            {
+                Assert.AreEqual(expected[n], actual[n]);
+            }
+        }
+
+        [TestMethod]
+        public void BitReversalMustReturnTheCorrectValuesThirtyTwo()
+        {
+            int N = 32;
+            var actual = fft.BitReversal(N);
+            var expected = new List<int>(new int[] { 0, 16, 8, 24, 4, 20, 12, 28, 2, 18, 10, 26, 6, 22, 14, 30, 1, 17, 9, 25, 5, 21, 13, 29, 3, 19, 11, 27, 7, 23, 15, 31 });
+
+            for (int n = 0; n < 32; n++)
+            {
+                Assert.AreEqual(expected[n], actual[n]);
+            }
+        }
+
+        [TestMethod]
+        public void BitReversalShouldNotApplyOverItself()
+        {
+            int N = 32;
+            var actual = fft.BitReversal(N);
+            actual = fft.BitReversal(N);
+            var expected = new List<int>(new int[] { 0, 16, 8, 24, 4, 20, 12, 28, 2, 18, 10, 26, 6, 22, 14, 30, 1, 17, 9, 25, 5, 21, 13, 29, 3, 19, 11, 27, 7, 23, 15, 31 });
+
+            for (int n = 0; n < 32; n++)
+            {
+                Assert.AreEqual(expected[n], actual[n]);
+            }
+        }
+
+        //[TestMethod]
+        //public void FFT_RadixTwoFirstStageMustReturnTheCorrectNumberOfValues()
+        //{
+
+        //}
+
+        //[TestMethod]
+        //public void FFT_RadixTwoFirstStageMustReturnTheCorrectValues()
+        //{
+
+        //}
+
+        //[TestMethod]
+        //public void FFT_RadixTwoMustReturnTheCorrectNumberOfValues()
+        //{
+
+        //}
+    }
+}
---
+++ DASPWorkstation/FFT_RadixTwo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DASPWorkstation
+{
+    public class FFT_RadixTwo
+    {
+        public List<int> BitReversal(int N)
+        {
+            var BR_Index = new List<int>(new int[N]);
+            BR_Index[0] = 0;
+            BR_Index[1] = 1;
+            int BR = 1;
+
+            for (int stage = 1; stage < Math.Log(N) / Math.Log(2); stage++)
+            {
+                BR = BR * 2;
+                for (int n = 0; n < BR; n++)
+                {
+                    BR_Index[n] = 2 * BR_Index[n];
+                    BR_Index[n + BR] = BR_Index[n] + 1;
+                }
+            }
+
+            return BR_Index;
+        }
+
+    }
+}
+
+
+//                        for (int n = 0; n<N; n++)
+//                        {
+//                            BR = BRar[n];
+//                            BRx[n] = x[BR];
+//                        }
+
+//                        //**************</Bit Reversal>*******************
+
+
+//                        //**************<First Stage>*********************
+//                        for (int n = 0; n<N / 2; n++)
+//                        {
+//                            even = BRx[n * 2];
+//                            odd = BRx[n * 2 + 1];
+//                            Xre[n * 2] = even + odd;
+//                            Xre[n * 2 + 1] = even + (odd* -1);
+//                        }
+//                        //**************</First Stage>********************
+
+
+//                        //**************<Second+ Stages>******************
+
+//                        Wdivide = 4;
+//                        DFTnum = N / 2;
+//                        m = 0;
+
+//                        for (int stage = 2; stage <= Math.Log(N) / Math.Log(2); stage++) // loop for each stage of fft 
+//                        {
+//                            m = 0;
+//                            for (int i = 0; i<DFTnum / 2; i++) // loop for each DFT in each stage 
+//                            {
+//                                for (int n = 0; n<Wdivide / 2; n++) // loop to seperate + and - indexes 
+//                                {
+//                                    reFH = Xre[m] + ((Xre[(Wdivide / 2) + m]) * (((float)Math.Cos(2 * pi * n) / Wdivide)));
+//                                    imFH = Xim[m] + ((Xim[Wdivide / 2 + m]) * (-1*((float)Math.Sin(2* pi* n/Wdivide))));
+
+//                                    reSH = (Xre[m + (Wdivide / 2)] * (((float)Math.Cos(2 * pi * (n + (Wdivide / 2))) / Wdivide))) + Xre[m];
+//                                    imSH = (Xim[Wdivide / 2 + m] * ((-1*((float)Math.Sin(2* pi*(Wdivide/2+m)/Wdivide))))) + Xim[m];  
+
+//                                    Xre[m] = reFH;
+//                                    Xim[m] = imFH;
+//                                    Xre[(Wdivide / 2) + m] = reSH;
+//                                    Xim[Wdivide / 2 + m] = imSH;
+//                                    m++;
+//                                }
+//                                m = m + (Wdivide / 2);
+//                            }
+//                            Wdivide = Wdivide* 2;
+//                            DFTnum = DFTnum / 2;
+//                        }
